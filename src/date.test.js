c2c1ab4e43834e48d3af6ef8590856b0cb9f4131
Atiquzzaman <atiquzzaman@gmail.com>
2019-09-14 23:35:26 +0600

    Added date formatter, date comparison

---
+++ src/date.test.js
@@ -0,0 +1,72 @@
+import * as utils from './utils'
+
+describe('Date related tests', function () {
+    it('it checks a valid date', function () {
+        const date = utils.getDate('3/5/2018')
+        expect(date.isValid()).toBe(true)
+    })
+
+    it('it checks a valid date with trailing 0', function () {
+        const date = utils.getDate('03/05/2018')
+        expect(date.isValid()).toBe(true)
+    })
+
+    it('it checks an invalid date empty string passed', function () {
+        const date = utils.getDate('')
+        expect(date.isValid()).toBe(false)
+    })
+
+    it('it check an invalid date', function () {
+        const date = utils.getDate('13/5/2018')
+        expect(date.isValid()).toBe(false)
+    })
+
+    it('it check an formatted date', function () {
+        const result = utils.changeDateFormat('3/5/2018')
+        expect(result).toBe('05/03/2018')
+    })
+
+    it('it check an another formatted date', function () {
+        const result = utils.changeDateFormat('12/15/2018')
+        expect(result).toBe('15/12/2018')
+    })
+
+    it('it check start date before end date', function () {
+        const startDate = utils.getDate('11/15/2018')
+        const endDate = utils.getDate('12/23/2018')
+        const result = utils.isStartDateSameOrBeforeEndDate(startDate, endDate)
+        expect(result).toBe(true)
+    })
+
+    it('it check start date after end date', function () {
+        const startDate = utils.getDate('3/15/2018')
+        const endDate = utils.getDate('2/23/2018')
+        const result = utils.isStartDateSameOrBeforeEndDate(startDate, endDate)
+        expect(result).toBe(false)
+    })
+
+    it('it check start date and end date are equal', function () {
+        const startDate = utils.getDate('3/15/2018')
+        const endDate = utils.getDate('3/15/2018')
+        const result = utils.isStartDateSameOrBeforeEndDate(startDate, endDate)
+        expect(result).toBe(true)
+    })
+
+    it('it check current date, start date and end date are equal', function () {
+        const date = utils.getDate('9/14/2019')
+        const startDate = utils.getDate('9/14/2019')
+        const endDate = utils.getDate('9/14/2019')
+
+        const result = utils.isDateBetween(date, startDate, endDate)
+        expect(result).toBe(true)
+    })
+
+    it('it check current date between start date and end date', function () {
+        const date = utils.getDate('10/14/2019')
+        const startDate = utils.getDate('9/14/2019')
+        const endDate = utils.getDate('11/14/2019')
+
+        const result = utils.isDateBetween(date, startDate, endDate)
+        expect(result).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,25 @@
 import numeral from 'numeral'
+import moment from 'moment'
+
+export const INPUT_DATE_FORMAT = 'M/D/YYYY'
+export const OUTPUT_DATE_FORMAT = 'DD/MM/YYYY'
 
 export const formatBudget = (amount) => {
     return numeral(amount).format('(0[.]0a)')
+}
+
+export const getDate = (date) => {
+    return moment(date, INPUT_DATE_FORMAT)
+}
+
+export const changeDateFormat = (dateStr) => {
+    return moment(dateStr, INPUT_DATE_FORMAT).format(OUTPUT_DATE_FORMAT)
+}
+
+export const isStartDateSameOrBeforeEndDate = (startDate, endDate) => {
+    return !endDate.isBefore(startDate)
+}
+
+export const isDateBetween = (date, startDate, endDate) => {
+    return date.isBetween(startDate, endDate, null, '[]');
 }
