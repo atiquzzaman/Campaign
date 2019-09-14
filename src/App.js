722cbaa2d959476c50c4673c968a234a0cf924ff
Atiquzzaman <atiquzzaman@gmail.com>
2019-09-14 16:50:58 +0600

    Simple table to render campaign dummy data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dummyList = [
+  { "id": 1, "name": "Divavu", "startDate": "9/19/2017", "endDate": "3/9/2018", "Budget": 88377 },
+  { "id": 2, "name": "Jaxspan", "startDate": "11/21/2017", "endDate": "2/21/2018", "Budget": 608715 },
+  { "id": 3, "name": "Miboo", "startDate": "11/1/2017", "endDate": "6/20/2017", "Budget": 239507 },
+  { "id": 4, "name": "Trilith", "startDate": "8/25/2017", "endDate": "11/30/2017", "Budget": 179838 },
+  { "id": 5, "name": "Layo", "startDate": "11/28/2017", "endDate": "3/10/2018", "Budget": 837850 },
+  { "id": 6, "name": "Photojam", "startDate": "7/25/2017", "endDate": "6/23/2017", "Budget": 858131 },
+  { "id": 7, "name": "Blogtag", "startDate": "6/27/2017", "endDate": "1/15/2018", "Budget": 109078 },
+  { "id": 8, "name": "Rhyzio", "startDate": "10/13/2017", "endDate": "1/25/2018", "Budget": 272552 },
+  { "id": 9, "name": "Zoomcast", "startDate": "9/6/2017", "endDate": "11/10/2017", "Budget": 301919 },
+  { "id": 10, "name": "Realbridge", "startDate": "3/5/2018", "endDate": "10/2/2017 ", "Budget": 505602 }
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Active</th>
+              <th>Budget</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dummyList.map((camp, key) => {
+              return (
+                <tr key={key}>
+                  <td>{camp.name}</td>
+                  <td>{camp.startDate}</td>
+                  <td>{camp.endDate}</td>
+                  <td></td>
+                  <td>{camp.Budget}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
