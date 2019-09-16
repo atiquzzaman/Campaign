276421e33ffa651cdd750c92431405126b710456
Atiquzzaman <atiquzzaman@gmail.com>
2019-09-16 21:07:52 +0600

    Added window function to invoke from browser console

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,40 @@ import DataTable from './components/DataTable'
 
 import './App.css'
 
-const dummyList = [
-  { "id": 1, "name": "Divavu", "startDate": "9/19/2017", "endDate": "3/9/2018", "Budget": 88377 },
-  { "id": 2, "name": "Jaxspan", "startDate": "11/21/2017", "endDate": "2/21/2018", "Budget": 608715 },
-  { "id": 3, "name": "Miboo", "startDate": "11/1/2017", "endDate": "6/20/2017", "Budget": 239507 },
-  { "id": 4, "name": "Trilith", "startDate": "8/25/2017", "endDate": "11/30/2019", "Budget": 179838 },
-  { "id": 5, "name": "Layo", "startDate": "11/28/2017", "endDate": "3/10/2018", "Budget": 837850 },
-  { "id": 6, "name": "Photojam", "startDate": "7/25/2017", "endDate": "6/23/2017", "Budget": 858131 },
-  { "id": 7, "name": "Blogtag", "startDate": "6/27/2017", "endDate": "1/15/2018", "Budget": 109078 },
-  { "id": 8, "name": "Rhyzio", "startDate": "10/13/2017", "endDate": "1/25/2018", "Budget": 272552 },
-  { "id": 9, "name": "Zoomcast", "startDate": "9/6/2017", "endDate": "11/10/2017", "Budget": 301919 },
-  { "id": 10, "name": "Realbridge", "startDate": "3/5/2018", "endDate": "10/2/2017 ", "Budget": 505602 }
-]
-
 class App extends React.Component {
-  render() {
-    const processedList = utils.processList(dummyList)
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaignList: []
+    }
+  }
+  componentDidMount() {
+    window.AddCampaigns = (newCampaigns) => {
+      try {
+        const processedList = utils.processList(newCampaigns)
 
+        if (processedList.goodList.length > 0) {
+          console.log(`${processedList.goodList.length} new campaign(s) has bee added.`)
+          this.setState(state => ({
+            campaignList: state.campaignList.concat(processedList.goodList)
+          }))
+        }
+        if (processedList.badList.length > 0) {
+          console.error(`Failed to add the following campaign(s): ${processedList.badList} `)
+          console.error(processedList.badList)
+          return
+        }
+      } catch (e) {
+        return e
+      }
+    }
+  }
+  render() {
     return (
       <div className="App">
         <h1>Campaign List</h1>
         <DataTable
-          dataList={processedList.goodList}
+          dataList={this.state.campaignList}
         />
       </div>
     );
